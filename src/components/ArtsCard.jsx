c5b15b1b866f847b6b493167ec9b7066f90a339a
tanimahmed07 <tanimislamt@gmail.com>
2025-11-11 22:34:43 +0600

    alhamdulillah dane finally

--- src/components/ArtsCard.jsx
+++ src/components/ArtsCard.jsx
@@ -8,30 +8,30 @@ const ArtsCard = ({ art }) => {
         <div>
             <div key={art._id} className="card border-2 border-gray-500 card-compact bg-base-100 shadow-xl transition transform hover:scale-102 hover:shadow-lg duration-300 ease-in-out">
                 <Fade
-                cascade={false}
-                triggerOnce={true}>
-                <figure>
-                    <img src={art.imageUrl} alt={art.title} className="h-48 w-full object-cover" />
-                </figure>
-                <div className="card-body">
-                    <Fade direction='down' triggerOnce={true}>
+                    cascade={false}
+                    triggerOnce={true}>
+                    <figure>
+                        <img src={art.imageUrl} alt={art.title} className="h-48 w-full object-cover" />
+                    </figure>
+                    <div className="card-body">
+                        <Fade direction='down' triggerOnce={true}>
 
-                    <h2 className="card-title">{art.title}</h2>
-                    <p>Artist: {art.artistName}</p>
-                    <p>Category: {art.category}</p>
-                    </Fade>
-                    <Fade direction='up' triggerOnce={true}>
-                    <p className="flex items-center gap-2 text-lg font-medium">
-                        <BiSolidLike size={24} className="text-blue-500" />
-                        Likes: {art.likes ?? 0}
-                    </p>
-                    <div className="card-actions justify-end">
-                        <Link to={`/art-details/${art._id}`} className="btn btn-primary btn-sm">
-                            View Details
-                        </Link>
+                            <h2 className="card-title">{art.title}</h2>
+                            <p>Artist: {art.artistName}</p>
+                            <p>Category: {art.category}</p>
+                        </Fade>
+                        <Fade direction='up' triggerOnce={true}>
+                            <p className="flex items-center gap-2 text-lg font-medium">
+                                <BiSolidLike size={24} className="text-blue-500" />
+                                Likes: {art.likes ?? 0}
+                            </p>
+                            <div className="card-actions justify-end">
+                                <Link to={`/art-details/${art._id}`} className="btn btn-primary btn-sm">
+                                    View Details
+                                </Link>
+                            </div>
+                        </Fade>
                     </div>
-                    </Fade>
-                </div>
                 </Fade>
             </div>
         </div>
--- src/pages/Home/CommunityHighlights.jsx
+++ src/pages/Home/CommunityHighlights.jsx
@@ -1,73 +1,93 @@
 import React from 'react';
 import { Fade } from "react-awesome-reveal";
+import { BiSolidLike } from 'react-icons/bi';
 const CommunityHighlights = () => {
     return (
             <section>
                 <h2 className="text-3xl font-bold mb-6">Community Highlights</h2>
                 <Fade cascade={false} delay={200} triggerOnce={true}>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                    <div className="card card-compact bg-base-100 shadow-xl transform hover:scale-102 hover:shadow-lg duration-300 ease-in-out border-2 border-gray-500">
-                        <figure>
-                            <img
-                                src="https://images.unsplash.com/photo-1507525428034-b723cf961d3e"
-                                alt="Sunset Bliss"
-                                className="h-56 w-full object-cover rounded-t-lg"
-                            />
-                        </figure>
-                        <div className="card-body">
-                            <h3 className="card-title text-lg font-bold">Sunset Bliss</h3>
-                            <p className="text-sm text-gray-500">by Alice Johnson</p>
-                            <Fade 
-                            direction='up'
-                            triggerOnce={true}>
-                            <div className="flex justify-between items-center mt-2">
-                                <span className="badge badge-info">Landscape</span>
-                                <button className="btn btn-primary btn-sm">View Details</button>
-                            </div>
-                            </Fade>
-                        </div>
-                    </div>
                     <div className="card card-compact bg-base-100 shadow-xl transition-shadow border border-gray-500 transform hover:scale-102 hover:shadow-lg duration-300 ease-in-out">
                         <figure>
                             <img
-                                src="https://images.unsplash.com/photo-1506744038136-46273834b3fb"
+                                src= 'https://www.rileystreet.com/cdn/shop/articles/shutterstock_410271079_1024x1024.jpg?v=1624398205'
                                 alt="Mountain Escape"
                                 className="h-56 w-full object-cover rounded-t-lg"
                             />
                         </figure>
-                        <div className="card-body">
-                            <h3 className="card-title text-lg font-bold">Mountain Escape</h3>
-                            <p className="text-sm text-gray-500">by John Doe</p>
-                            <Fade 
-                            direction='up'
-                            triggerOnce={true} >
-                            <div className="flex justify-between items-center mt-2">
-                                <span className="badge badge-info">Adventure</span>
-                                <button className="btn btn-primary btn-sm">View Details</button>
+                         <div className="card-body">
+                        <Fade direction='down' triggerOnce={true}>
+                            <h2 className="card-title">Floating Flora</h2>
+                            <p>Artist: Mahi</p>
+                            <p>Category:  Acrylic</p>
+                        </Fade>
+                        <Fade direction='up' triggerOnce={true}>
+                            <p className="flex items-center gap-2 text-lg font-medium">
+                                <BiSolidLike size={24} className="text-blue-500" />
+                                Likes: 46
+                            </p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary btn-sm">
+                                    View Details
+                                </button>
                             </div>
-                            </Fade>
-                        </div>
+                        </Fade>
+                    </div>
                     </div>
                     <div className="card card-compact bg-base-100 shadow-xl transition-shadow border border-gray-500 transform hover:scale-102 hover:shadow-lg duration-300 ease-in-out">
                         <figure>
                             <img
-                                src="https://images.unsplash.com/photo-1517836357463-d25dfeac3438"
-                                alt="Ocean Calm"
+                                src= 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVFrrROFtBGKe0EaqzUk58uoEx54MXg3KDYJy6sIf1_32w8035RMRxDuPyhtEHneAOsIo&usqp=CAU'
+                                alt="Mountain Escape"
                                 className="h-56 w-full object-cover rounded-t-lg"
                             />
                         </figure>
-                        <div className="card-body">
-                            <h3 className="card-title text-lg font-bold">Ocean Calm</h3>
-                            <p className="text-sm text-gray-500">by Emma Williams</p>
-                              <Fade 
-                            direction='up'
-                            triggerOnce={true} >
-                            <div className="flex justify-between items-center mt-2">
-                                <span className="badge badge-info">Seascape</span>
-                                <button className=" btn btn-outline-primary btn-primary btn-sm">View Details</button>
+                         <div className="card-body">
+                        <Fade direction='down' triggerOnce={true}>
+                            <h2 className="card-title">Twin Blooms in the Water</h2>
+                            <p>Artist: Sadiya Islam</p>
+                            <p>Category: Watercolor</p>
+                        </Fade>
+                        <Fade direction='up' triggerOnce={true}>
+                            <p className="flex items-center gap-2 text-lg font-medium">
+                                <BiSolidLike size={24} className="text-blue-500" />
+                                Likes: 
+                            </p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary btn-sm">
+                                    View Details
+                                </button>
                             </div>
-                            </Fade>
-                        </div>
+                        </Fade>
+                    </div>
+                    </div>
+                    <div className="card card-compact bg-base-100 shadow-xl transition-shadow border border-gray-500 transform hover:scale-102 hover:shadow-lg duration-300 ease-in-out">
+                        <figure>
+                            <img
+                                src= 'https://i.pinimg.com/736x/8e/68/7a/8e687a252e5607f593b4d57665b3d69a.jpg'
+                                alt="Mountain Escape"
+                                className="h-56 w-full object-cover rounded-t-lg"
+                            />
+                        </figure>
+                         <div className="card-body">
+                        <Fade direction='down' triggerOnce={true}>
+
+                            <h2 className="card-title">Enchanted Forest</h2>
+                            <p>Artist: Eva Ahamed</p>
+                            <p>Category: Watercolor</p>
+                        </Fade>
+                        <Fade direction='up' triggerOnce={true}>
+                            <p className="flex items-center gap-2 text-lg font-medium">
+                                <BiSolidLike size={24} className="text-blue-500" />
+                                Likes: 
+                            </p>
+                            <div className="card-actions justify-end">
+                                <button className="btn btn-primary btn-sm">
+                                    View Details
+                                </button>
+                            </div>
+                        </Fade>
+                    </div>
                     </div>
                 </div>
                     </Fade>
